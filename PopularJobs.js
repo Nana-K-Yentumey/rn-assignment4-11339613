ba71850557f38b1d9634e8a2dd258b01e6a15c5c
death-inspires-me <nana.esseney@gmail.com>
2024-06-17 15:48:42 +0000

    Finished the popular jobs functional component

--- PopularJobs.js
+++ PopularJobs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList, ScrollView, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import PopularCards from './PopularCards';
 
 const PopularJobs = () => {
@@ -8,14 +8,14 @@ const PopularJobs = () => {
   <View style={styles.container}>
     <Text style={styles.title}>Popular Jobs</Text>
   <PopularCards  imageSource={require('./assets/bgking.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
-  <PopularCards  imageSource={require('./assets/fcbook.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
-  <PopularCards  imageSource={require('./assets/beatz.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
-  <PopularCards  imageSource={require('./assets/bgking.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
-  <PopularCards  imageSource={require('./assets/bgking.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
-  <PopularCards  imageSource={require('./assets/bgking.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
-  <PopularCards  imageSource={require('./assets/bgking.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
-  <PopularCards  imageSource={require('./assets/bgking.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
-  <PopularCards  imageSource={require('./assets/bgking.png')}  JobTitle={'Jr Executive'} salary={'$96,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
+  <PopularCards  imageSource={require('./assets/fcbook.png')}  JobTitle={'Product Manager'} salary={'$84,000/y'} employer={'Facebook'} location={'Florida, US'} />
+  <PopularCards  imageSource={require('./assets/beatz.png')}  JobTitle={'Product Manager'} salary={'$86,000/y'} employer={'Beats'} location={'Florida, US'} />
+  <PopularCards  imageSource={require('./assets/googlelogo.png')}  JobTitle={'Data Analyst'} salary={'$100,000/y'} employer={'Google'} location={'New York, US'} />
+  <PopularCards  imageSource={require('./assets/amazonlogo.png')}  JobTitle={'Delivery Driver'} salary={'$56,000/y'} employer={'Amazon'} location={'Los Angeles, US'} />
+  <PopularCards  imageSource={require('./assets/activisionlogo.png')}  JobTitle={'Jr Developer'} salary={'$96,000/y'} employer={'Activision'} location={'Los Santos, US'} />
+  <PopularCards  imageSource={require('./assets/bgking.png')}  JobTitle={'Customer Service'} salary={'$36,000/y'} employer={'Burger King'} location={'Los Angeles, US'} />
+  <PopularCards  imageSource={require('./assets/fcbook.png')}  JobTitle={'Jr Developer'} salary={'$76,000/y'} employer={'Facebook'} location={'Las Vegas, US'} />
+  <PopularCards  imageSource={require('./assets/googlelogo.png')}  JobTitle={'AI Specialist'} salary={'$196,000/y'} employer={'Google'} location={'Vice City, US'} />
 
 </View>
 
